574f366b9d55527509799b4d0adf5971f80338e7
mabbason <miles.abbason@gmail.com>
2022-04-28 08:51:06 -0400

    Added pagination options and prev/next buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component }  from 'react';
+import React, { useState }  from 'react';
 import Table from './components/Table'
 import './App.css';
 import data from './data'
 const { routes, getAirlineById, getAirportByCode } = data
 
 const App = () => {
+  const [ perPage, setPerPage ] = useState(25)
+
   const columns = [
     {name: 'Airline', property: 'airline'},
     {name: 'Source Airport', property: 'src'},
@@ -18,6 +20,10 @@ const App = () => {
       return getAirportByCode(value)
     }
   }
+
+  const setRowsPerPage = (event) => {
+    setPerPage(Number(event.target.value))
+  }
   
   return (
   <div className="app">
@@ -29,8 +35,17 @@ const App = () => {
         Welcome to the app!
       </p>
     </section>
-    <Table className="routes-table" columns={columns} rows={routes} format={formatValue} />
-    
+    <label>
+      Rows Per Page:
+      <select value={perPage} onChange={setRowsPerPage}>
+        <option value={10}>10</option>
+        <option value={25}>25</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
+        {/* <option value="all">All</option> */}
+      </select>
+    </label>
+    <Table className="routes-table" columns={columns} rows={routes} format={formatValue} rowsPerPage={perPage}/>
   </div>
   )
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
-const Table = ({ className, columns, rows, format }) => {
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const Table = ({ className, columns, rows, format, rowsPerPage }) => {
+  const [ page, setPage ] = useState(0)
+  // let rowsPerPage = 25
+  console.log('Table', rowsPerPage)
+  let startingRow = rowsPerPage * page
+
+  const handleSetPage = (newPageNum) => {
+    if (newPageNum < 0) {
+      setPage(0)
+    } else if (newPageNum * rowsPerPage + 1 > rows.length) {
+      setPage(newPageNum - 1)
+    } else {
+      setPage(newPageNum)
+    }
+  }
+
+  const endingPage = (givenRow) => {
+    return givenRow > rows.length ? rows.length : givenRow
+  }
+
   return (
+    <>
+    <Button onClick={() => handleSetPage(page - 1)} text="Previous" />
+    <h4>Showing Routes {`${startingRow + 1} - ${endingPage(startingRow + rowsPerPage)}`} of {rows.length}</h4>
+    <Button onClick={() => handleSetPage(page + 1)} text="Next" />
+
     <table>
       <tr>
-        {columns.map(c => <td>{c.name}</td>)}
+        {columns.map(c => <td><strong>{c.name}</strong></td>)}
       </tr>
-      {rows.map(r => <tr>
+      {rows.slice(startingRow, startingRow + rowsPerPage).map((r, idx) => <tr>
+          {/* <td>{idx} </td> */}
           <td>{format('airline', r.airline)}</td>
           <td>{format('src', r.src)}</td>
           <td>{format('dest', r.src)}</td>
         </tr>)
        }
     </table>
+    </>
   )
 }
 
